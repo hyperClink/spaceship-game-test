ec6785bab769676e2adb13527d854914b3333b0e
hyperClink <45831360+hyperClink@users.noreply.github.com>
2018-12-13 21:00:44 +0300

    Bullet pattern fixes
    Tweaked bullet pattern speeds and decay, fixed overlaying conditions and bullets are affected by ship speed now

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 //start variables
-var yp = 10;
+var yp = 100;
 var xp = 20;
 var keydownkey='null';
 var keyupkey='null';
@@ -13,12 +13,13 @@ var left=false;
 var right=false;
 var shoot=false;
 var bn=0;
-var i=1;
+var cl=1;
 var speed = 0;
+var xspeed = 0;
+var yspeed = 0;
 
 //creating bullet array
 var bullets=[];
-bullets[0] = {x:null, y:900, accel:null, scale:null, decay:false};
 
 //img creation
 var ship = new Image();
@@ -65,25 +66,29 @@ function keydownkey1(keydownkey) {
     }
 }
 
-
+//check for keys which are up
 function keyupkey1(keyupkey) {
   keyupkey=keyupkey.key;
     switch(keyupkey){
 
       case 'w':
         up = false;
+        yspeed = 0;
       break;
 
       case 's':
         down = false;
+        yspeed = 0;
       break;
 
       case 'a':
         left = false;
+        xspeed = 0;
       break;
 
       case 'd':
         right = false;
+        xspeed = 0;
       break;
 
       case ' ':
@@ -96,28 +101,50 @@ function keyupkey1(keyupkey) {
 
   }
 
+  //sets up the bullet
+    fireBullet = function() {
+      cl++;
+      if (cl%3==0){
+      bullets.push({x:xp+43, y:yp+23, accel:Math.random()*2, scale:0, timer:0, xspeed:xspeed/3, yspeed:yspeed/3, flipCoin:randomInt(-1 ,1)});
+        };
+      };
+
+  //random integer
+   randomInt = function(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random()*(max-min+1))+min;
+   };
+
 //engine
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(ship, xp, yp);
   requestAnimationFrame(draw);
 
-if (speed<2 && (up==true || down==true || left==true || right==true)) {speed += 0.3};
+//ship acceleration
+if (speed<2 && (up==true || down==true || left==true || right==true)) {speed += 0.16};
+if (up==false && down==false && left==false && right==false && speed>0){speed =0}
 
+//ship keydown actions
   if (up == true){
     yp -=4+speed;
+    yspeed = -(4+speed);
   }
 
   if (down == true){
     yp +=4+speed;
+    yspeed = 4+speed;
   }
 
   if (left == true){
     xp -=5+speed;
+    xspeed = -(4+speed);
   }
 
   if (right == true){
     xp +=5+speed;
+    xspeed = 4+speed;
   }
 
   if (shoot == true){
@@ -131,33 +158,30 @@ if (speed<2 && (up==true || down==true || left==true || right==true)) {speed += 
         };
     };
 
-//draw rocket good
+//draw bullets
   for(var i=0; i<bullets.length; i++) {
       ctx.drawImage(bullet, bullets[i].x, bullets[i].y, bullets[i].scale/5+10, bullets[i].scale/5+10);
-      bullets[i].x +=7+bullets[i].accel;
-      bullets[i].y +=bullets[i].accel/10;
+      bullets[i].x +=9+bullets[i].accel+bullets[i].xspeed;
+      bullets[i].y +=(bullets[i].accel/10)*bullets[i].flipCoin;
       bullets[i].accel +=0.1;
-      if (bullets[i].accel < 5 && bullets[i].decay == false) {
-        bullets[i].scale = -bullets[i].accel;
+      bullets[i].timer++;
+
+      //size-time modification
+      if (bullets[i].timer < 40) {
+        bullets[i].scale = -bullets[i].timer/50;
       }
-      if (bullets[i].accel > 5 || bullets[i].decay == true) {
-        bullets[i].decay = true;
-        bullets[i].y += 4;
-        bullets[i].accel -=0.8;
-        bullets[i].scale -=1;
+
+      //bullet full decay
+      if (bullets[i].timer > 40-randomInt(0, 5)) {
+        bullets[i].y += 6*bullets[i].flipCoin*Math.random();
+        bullets[i].scale -=5;
+        bullets[i].accel -=Math.random()*bullets[i].timer/10;
       };
 
   };
 
-//fires the bullet
-  fireBullet = function() {
-        bullets.push({x:xp+43, y:yp+23, accel:Math.random(), scale:0, decay:false});
-
-      };
+  };
 
 
-  console.log(speed);
-  if (up==false && down==false && left==false && right==false && speed>0){speed -=0.1}
-};
 
 draw();
