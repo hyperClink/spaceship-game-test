9afc902035b8a46af06dcb7699e417200fe945f3
hyperClink <45831360+hyperClink@users.noreply.github.com>
2018-12-22 13:19:14 +0300

    Enemies added
    Tidied up code and enemies added, shift and caps don't break controls anymore, arrow controls added, unused cvariables removed

--- code.js
+++ code.js
@@ -2,57 +2,77 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-//start variables
+//ship start variables
 var yp = 100;
 var xp = 20;
-var keydownkey='null';
-var keyupkey='null';
+var speed = 0;
+
+//bulletT1 start variables
+var xspeed = 0;
+var yspeed = 0;
+var BulletTimer=0;
+var BulletRate=1;
+
+//control variables
 var up=false;
 var down=false;
 var left=false;
 var right=false;
 var shoot=false;
-var bn=0;
-var cl=1;
-var speed = 0;
-var xspeed = 0;
-var yspeed = 0;
+
+var keydownkey='null';
+var keyupkey='null';
+
+//enemy variables
+var EnemyTimer = 0;
+var EnemyRate = 1;
+
 
 //creating bullet array
 var bullets=[];
+var enemies=[];
 
 //img creation
 var ship = new Image();
 var bg = new Image();
 var bullet = new Image();
+var enemy1 = new Image();
 
 //sources of images
 ship.src='img/ship.png';
 bg.src='img/bg2.jpg';
 bullet.src='img/bullet_custom.png';
+enemy1.src='img/enemy.png';
 
 //key check
 document.addEventListener("keydown", keydownkey1);
 document.addEventListener('keyup', keyupkey1);
 
-function keydownkey1(keydownkey) {
+//key down check
+function keydownkey1(keyd) {
+    switch(keyd.key){
 
-  keydownkey=keydownkey.key
-    switch(keydownkey){
-
+      case 'W':
       case 'w':
+      case 'ArrowUp':
         up = true;
       break;
 
+      case 'S':
       case 's':
+      case 'ArrowDown':
         down = true;
       break;
 
+      case 'A':
       case 'a':
+      case 'ArrowLeft':
         left = true;
       break;
 
+      case 'D':
       case 'd':
+      case 'ArrowRight':
         right = true;
       break;
 
@@ -67,26 +87,33 @@ function keydownkey1(keydownkey) {
 }
 
 //check for keys which are up
-function keyupkey1(keyupkey) {
-  keyupkey=keyupkey.key;
-    switch(keyupkey){
+function keyupkey1(keyu) {
+    switch(keyu.key){
 
+      case 'W':
       case 'w':
+      case 'ArrowUp':
         up = false;
         yspeed = 0;
       break;
 
+      case 'S':
       case 's':
+      case 'ArrowDown':
         down = false;
         yspeed = 0;
       break;
 
+      case 'A':
       case 'a':
+      case 'ArrowLeft':
         left = false;
         xspeed = 0;
       break;
 
+      case 'D':
       case 'd':
+      case 'ArrowRight':
         right = false;
         xspeed = 0;
       break;
@@ -101,13 +128,22 @@ function keyupkey1(keyupkey) {
 
   }
 
+
   //sets up the bullet
     fireBullet = function() {
-      cl++;
-      if (cl%3==0){
+      BulletTimer++;
+      if (BulletTimer%BulletRate==0) {
       bullets.push({x:xp+43, y:yp+23, accel:Math.random()*2, scale:0, timer:0, xspeed:xspeed/3, yspeed:yspeed/3, flipCoin:randomInt(-1 ,1)});
         };
       };
+
+  //adds enemies to the array
+    spawnEnemy = function() {
+      EnemyTimer++;
+      if (EnemyTimer%EnemyRate==0) {
+      enemies.push({x:canvas.width, y:randomInt(0,512)});
+    };
+  };
 
   //random integer
    randomInt = function(min, max) {
@@ -116,69 +152,100 @@ function keyupkey1(keyupkey) {
      return Math.floor(Math.random()*(max-min+1))+min;
    };
 
+
 //engine
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(ship, xp, yp);
   requestAnimationFrame(draw);
+  spawnEnemy();
 
 //ship acceleration
 if (speed<2 && (up==true || down==true || left==true || right==true)) {speed += 0.16};
-if (up==false && down==false && left==false && right==false && speed>0){speed =0}
+if (up==false && down==false && left==false && right==false && speed>0) {speed =0};
 
 //ship keydown actions
-  if (up == true){
+
+  if (up == true && yp>0){
     yp -=4+speed;
     yspeed = -(4+speed);
   }
 
-  if (down == true){
+  if (down == true && yp+ship.height<canvas.height){
     yp +=4+speed;
     yspeed = 4+speed;
   }
 
-  if (left == true){
+  if (left == true && xp>0){
     xp -=5+speed;
     xspeed = -(4+speed);
   }
 
-  if (right == true){
+  if (right == true && xp+ship.width<d canvas.width){
     xp +=5+speed;
     xspeed = 4+speed;
   }
 
   if (shoot == true){
         fireBullet();
-      }
+  }
 
-//remove rocket somehow
+//remove rockets
   for(var i=0; i<bullets.length; i++) {
         if(bullets[i].x >canvas.width || bullets[i].scale/5+10<0) {
             bullets.splice(i--, 1);
         };
     };
 
-//draw bullets
+//remove enemies
+  for(var i=0; i<enemies.length; i++) {
+        if(enemies[i].x+enemy1.width <0){
+            enemies.splice(i--, 1);
+        };
+    };
+
+//enemies-bullets collision
+  for(var i=0; i<bullets.length && bullets.length != 'undefined' && bullets.length != 0; i++) {
+    for(var n=0; n<enemies.length && enemies.length != 'undefined' && enemies.length != 0; n++)
+      if (i >= 0) {
+        if(bullets[i].x>enemies[n].x &&
+           bullets[i].x-enemy1.width<enemies[n].x &&
+           bullets[i].y>enemies[n].y &&
+           bullets[i].y<enemies[n].y+enemy1.height){
+             enemies.splice(n--, 1);
+             bullets.splice(i--, 1);
+           };
+        };
+    };
+
+//bullets logic
   for(var i=0; i<bullets.length; i++) {
       ctx.drawImage(bullet, bullets[i].x, bullets[i].y, bullets[i].scale/5+10, bullets[i].scale/5+10);
-      bullets[i].x +=9+bullets[i].accel+bullets[i].xspeed;
+      bullets[i].x +=8+bullets[i].accel+bullets[i].xspeed;
       bullets[i].y +=(bullets[i].accel/10)*bullets[i].flipCoin;
       bullets[i].accel +=0.1;
       bullets[i].timer++;
 
       //size-time modification
-      if (bullets[i].timer < 40) {
+      if (bullets[i].timer < 32) {
         bullets[i].scale = -bullets[i].timer/50;
       }
 
-      //bullet full decay
-      if (bullets[i].timer > 40-randomInt(0, 5)) {
+      //bullet decay
+      if (bullets[i].timer > 32-randomInt(0, 5)) {
         bullets[i].y += 6*bullets[i].flipCoin*Math.random();
         bullets[i].scale -=5;
         bullets[i].accel -=Math.random()*bullets[i].timer/10;
       };
 
   };
+
+  //draw enemies
+    for(var i=0; i<enemies.length; i++) {
+      ctx.drawImage(enemy1, enemies[i].x, enemies[i].y);
+      enemies[i].x-=1;
+    };
+
 
   };
 
